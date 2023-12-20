4546beb82ddd21ad9246532e2c5a5cd201d63809
hiiiiiilim <vnfwlq07@naver.com>
2023-12-20 10:48:37 +0900

    .

---
+++ src/component/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <Footer>
+            <p>&copy; 2023 My Website. All right reserved.</p>
+            <p><a href="/">git URL</a></p>
+        </Footer>
+    )
+}
+
+export default Footer;
