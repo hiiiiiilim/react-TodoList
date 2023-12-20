63e40d33a1064227ea14d1c63e82e49ac6270a41
hiiiiiilim <vnfwlq07@naver.com>
2023-12-20 10:16:06 +0900

    .

--- src/component/App.js
+++ src/component/App.js
@@ -1,1 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+function App(){
+    return(
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">home</Link>
+                        </li>
+                            <Link to="/todo">할 일 목록</Link>
+                    </ul>
+                </nav>
+                <hr/>
+                <Routes>
+                    <Route path="/todo" element={<TodoList/>}></Route>
+                </Routes>
+            </div>
+        </Router>
+    )
+}
+
+export default App;
