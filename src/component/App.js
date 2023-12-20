3e703de79545d39c26741f75cf1ee0ab4becf4f7
hiiiiiilim <vnfwlq07@naver.com>
2023-12-20 10:19:24 +0900

    .

--- src/component/App.js
+++ src/component/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import TodoList from './component/TodoList'
 
 function App(){
     return(
